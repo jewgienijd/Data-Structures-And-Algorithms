676f6e9880b50f1125fc23f003216e635b7ec6ba
Jewgienij Danilczik <85256624+jewgienijd@users.noreply.github.com>
2026-02-03 23:13:45 +0100

    Time: 1 ms (49.18%) | Memory: 57.2 MB (74.64%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -3,14 +3,14 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-  let i = 0;
-  while (i < nums.length) {
-    if (nums[i] == nums[i+1]) {
-      nums.splice(i, 1);
-    } else {
-      i++;
+  let k = 1;
+
+  for (let i=1; i < nums.length; i++) {
+    if (nums[i] !== nums[k-1]) {
+        nums[k] = nums[i];
+        k++;
     }
-    
   }
-  return nums.length
+  
+  return k;
 };
